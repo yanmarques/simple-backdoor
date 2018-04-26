5ef1014ba3bcba68cd6d37f95e0039b49e8b71e5
= <=>
2018-04-26 17:54:23 -0300

    refactor(responses) - Add BaseResponse class as responses base class

--- responses.py
+++ responses.py
@@ -12,12 +12,37 @@ The normal response codes will be of sucess and error. Althought the socket is a
 that will be handled with a diferent response code, as it must be stored on file system or in memory for
 short use case.
 
+The responses uses BaseResponse as base class. The BaseResponse helps you to create responses and is
+used to instruct socket to handle response/requests.
+
 """
 
 from protocol import Protocol
-from requests import BaseRequest
 
-class Response(BaseRequest):
+class BaseResponse(abstract):
+    """BaseResponse class."""
+
+    def __init__(self, content, code):
+        """
+        Create response object with content and response code. No parameters allowed so far.
+
+        :param content: The response content.
+        :param code: The significant response code.
+        :return: The BaseResponse object.
+        """
+        self.__content = content
+        self.__code = code
+
+    @property
+    def content(self):
+        """Get response content as bytes."""
+        return self.__content
+
+    def get_raw(self):
+        """Get raw content to be sent over socket."""
+        return Protocol.to_response(self.__code, self.__content)
+
+class Response(BaseResponse):
     """The normal response with the content."""
     def __init__(self, content, code=None):
         """
@@ -27,19 +52,4 @@ class Response(BaseRequest):
         :param code: The significant response code.
         :return: Class object.
         """
-        self.__content = content
-        self.__code = code if code else Protocol.SUCCESS_CODE
-
-    @property
-    def content(self):
-        """Get response content as bytes."""
-        return self.__content
-
-    @property
-    def params(self):
-        """Get the response parameters."""
-        return None
-
-    def get_raw(self):
-        """Get raw content to be sent over socket."""
-        return Protocol.to_response(self.__code, self.__content)
+        BaseResponse.__init__(content, code if code else Protocol.SUCCESS_CODE)
