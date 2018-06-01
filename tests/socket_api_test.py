795c686716fb5c3de0c75cf98f3b62694a2d9638
Yan Marques de Cerqueira <marques_yan@outlook.com>
2018-06-01 15:24:13 -0300

    test - Add close and is_alive test

--- tests/socket_api_test.py
+++ tests/socket_api_test.py
@@ -12,31 +12,79 @@ from simple_backdoor.socket_api import SocketApi
 from simple_backdoor.socket_factory import SocketFactory
 
 SERVER_PORT = 9999
-ON_CONNECTED = 'connected' # Message sent from server to client when connected.
-ON_CLIENT_CLOSE = 'finish' # Message sent client to server.
+MESSAGE = 'connected' # Message sent from server to client when connected.
 
 class SocketApiTest(unittest.TestCase):
     def setUp(self):
+        # Set the available server port for each test.
         SERVER_PORT = self.get_available_port()
 
     def test_send_method(self):
-        def serve(unittestobj, server):
+        def serve(server, unittestobj):
             server = SocketApi(server.accept()[0])
-            unittestobj.assertEqual(server.recv(), ON_CONNECTED)
+            unittestobj.assertEqual(server.recv(), MESSAGE)
             server.close()
 
+        # Create the socket object.
+        server = self.create_server(serve, self)
+
+        client = SocketApi(SocketFactory.client('127.0.0.1', SERVER_PORT))
+        client.send(MESSAGE)
+        client.close()
+        server.close()
+
+    def test_recv_method(self):
+        def serve(server):
+            server = SocketApi(server.accept()[0])
+            server.send(MESSAGE)
+            server.close()
+
+        # Create the socket object.
+        server = self.create_server(serve)
+
+        client = SocketApi(SocketFactory.client('127.0.0.1', SERVER_PORT))
+        self.assertEqual(client.recv(), MESSAGE)
+        client.close()
+        server.close()
+
+    def test_close_method(self):
+        def serve(server):
+            server = SocketApi(server.accept()[0])
+            server.close()
+
+        # Create the socket object.
+        server = self.create_server(serve)
+
+        client = SocketApi(SocketFactory.client('127.0.0.1', SERVER_PORT))
+        client.close()
+        self.assertFalse(client.is_alive())
+        server.close()
+
+    def test_is_alive_method(self):
+        def serve(server):
+            import time
+            server = SocketApi(server.accept()[0])
+
+            # Sleeps for 2 seconds.
+            time.sleep(1)
+            server.close()
+
+        # Create the socket object.
+        server = self.create_server(serve)
+
+        client = SocketApi(SocketFactory.client('127.0.0.1', SERVER_PORT))
+        self.assertTrue(client.is_alive())
+        client.close()
+        self.assertFalse(client.is_alive())
+        server.close()
+
+    def create_server(self, callback, args = []):
         # Create the socket object.
         server = SocketFactory.server(SERVER_PORT)
 
         # Listen connections on a coroutine.
-        threading.Thread(target=serve, args=(self, server,)).start()
-
-        client = SocketApi(SocketFactory.client('127.0.0.1', SERVER_PORT))
-        client.send(ON_CONNECTED)
-        client.close()
-        server.close()
-
-    # def test_recv_method(self):
+        threading.Thread(target=callback, args=(server, *args)).start()
+        return server
 
     def get_available_port(self):
         def check_availability(port):
